286171548e652a37b0d034f2fca405b1c6b49c03
Romain Testard <romain.testard@bluenove.com>
2021-01-10 12:22:21 +0100

    I have implement GET request

---
+++ controller/video-controller.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"github.com/TestardR/Go-Gin-CRUD/entity"
+	"github.com/TestardR/Go-Gin-CRUD/service"
+	"github.com/gin-gonic/gin"
+)
+
+type VideoController interface {
+	Save(ctx *gin.Context) entity.Video
+	FindAll() []entity.Video
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+func New(service service.VideoService) VideoController {
+	return &controller {
+		service: service,
+	}
+} 
+
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) entity.Video {
+	var video entity.Video
+	ctx.BindJSON(&video)
+	c.service.Save(video)
+	return video
+} 
